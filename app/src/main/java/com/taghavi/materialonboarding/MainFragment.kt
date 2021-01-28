a88c139c9b6145dce3ab1f41dd0f07c86c480a85
EhsanTgv <EhsanTgv@gmail.com>
2021-01-28 23:33:30 +0330

    Implement MainFragment

---
+++ app/src/main/java/com/taghavi/materialonboarding/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.taghavi.materialonboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnOnboarding.setOnClickListener {
+            OnboardingFragment().show(childFragmentManager, "foo")
+        }
+    }
+}
